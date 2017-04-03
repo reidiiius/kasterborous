272832c7cd1d766d1b0bbd9b235ca8b5f04241c6
Reid Netterville III <reidiiius@gmail.com>
2017-04-02 19:34:37 -0500

    170402.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# kasterborous
+# Kasterborous
 Eye of Harmony
+
+---
+
+### Download
+
+    git clone https://github.com/reidiiius/kasterborous.git
+
+---
+
+### Usage
+command line
+
+    cd kasterborous
+
+    octave -qf gallifrey.m | less
+
+or filter with grep
+
+    octave -qf gallifrey.m | grep -B1 -A8 'n0'
+
+set execute permission
+
+    chmod u+x gallifrey.m
+
+    ./gallifrey.m | less
+
+---
+
+### Distribution
+MIT License
+
---
+++ gallifrey.m
@@ -0,0 +1,407 @@
+#!/usr/bin/octave -q
+
+      Rod(1).j2 = 'HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ AgUr ____ FePu ';
+      Rod(1).j3 = 'HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb ____ FeUr ';
+      Rod(1).j5 = 'PbCu ____ AuSn ____ AgHg TiFe FeTi ____ ____ SnAu ____ CuPb ';
+      Rod(1).j6 = 'HgAu ____ SnPb ____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp ';
+      Rod(1).k1 = '____ FeUr HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb ';
+      Rod(1).k2 = 'NpCu ____ ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ';
+      Rod(1).k5 = 'UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb ';
+      Rod(1).k6 = 'HgAg ____ SnAu ____ CuPb PbCu ____ AuSn ____ ____ TiFe FeTi ';
+      Rod(1).n0 = 'HgCu ____ SnSn ____ CuHg PbFe ____ AuAg ____ AgAu ____ FePb ';
+     Rod(1).j17 = '____ ____ SnAu ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi ';
+     Rod(1).j23 = 'HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ AgUr ____ FePu ';
+     Rod(1).j25 = 'FeCu HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb ';
+     Rod(1).j26 = 'HgHg PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu ';
+     Rod(1).j36 = 'HgAu ____ SnPb UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp ';
+     Rod(1).j56 = 'UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ____ ____ SnPb ';
+     Rod(1).k12 = '____ AgUr ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ';
+     Rod(1).k15 = '____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb ';
+     Rod(1).k25 = 'NpCu ____ ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ';
+     Rod(1).k26 = 'HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb FeCu ';
+     Rod(1).k34 = 'PbCu ____ AuSn ____ ____ TiFe FeTi HgAg ____ SnAu ____ CuPb ';
+     Rod(1).k56 = 'HgAu ____ SnPb ____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp ';
+    Rod(1).j236 = 'HgHg PuFe ____ UrAg ____ PbAu ____ AuPb NpCu ____ ____ FePu ';
+    Rod(1).j256 = 'FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb ';
+    Rod(1).j2k5 = 'FeCu HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb ';
+    Rod(1).j2k6 = 'HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ ____ TiSn FePu ';
+    Rod(1).j2y3 = 'HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ AgUr ____ FePu ';
+    Rod(1).j3k5 = 'NpCu ____ TiSn FePu ____ PuFe ____ ____ CuNp PbAu ____ AuPb ';
+    Rod(1).j3k6 = 'HgTi ____ SnNp UrAu ____ PbPb ____ AuUr ____ ____ TiHg FeFe ';
+    Rod(1).j5y6 = 'PbCu ____ AuSn ____ AgHg TiFe FeTi HgAg ____ ____ ____ CuPb ';
+    Rod(1).k125 = '____ AgUr ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ';
+    Rod(1).k1j5 = '____ AuUr NpSn ____ TiHg FeFe HgTi ____ ____ UrAu ____ PbPb ';
+    Rod(1).k1j6 = '____ PuFe SnTi ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu ';
+    Rod(1).k256 = 'HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb FeCu ';
+    Rod(1).k2j5 = 'NpCu ____ ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ';
+    Rod(1).k2j6 = 'HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ FeCu ';
+    Rod(1).k2x1 = '____ ____ TiSn FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ';
+    Rod(1).k6x5 = 'HgAg ____ SnAu ____ CuPb PbCu ____ ____ ____ AgHg TiFe FeTi ';
+    Rod(1).n167 = 'NpCu ____ ____ FePu ____ PuFe SnTi ____ CuNp PbAu ____ AuPb ';
+    Rod(1).n345 = '____ PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ TiSn FePu ';
+    Rod(1).n5y2 = 'HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb FeCu ';
+    Rod(1).n6x2 = 'FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ ';
+   Rod(1).j17k2 = '____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb TiCu FeMn ';
+   Rod(1).j17y2 = 'HgAg ____ ____ ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi ';
+   Rod(1).j23k6 = 'HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ ____ TiSn FePu ';
+   Rod(1).j25y6 = 'TiCu FeMn ____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb ';
+   Rod(1).j26y3 = 'HgHg PuFe SnTi ____ ____ PbAu ____ AuPb NpCu ____ ____ FePu ';
+   Rod(1).j2k34 = 'TiCu FeMn ____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb ';
+   Rod(1).j2k56 = 'HgHg PuFe ____ ____ CuNp PbAu ____ ____ NpCu ____ TiSn FePu ';
+   Rod(1).j34k6 = 'HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ TiCu FeMn ';
+   Rod(1).j56y7 = 'UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp HgAu ____ ____ ';
+   Rod(1).k12j5 = '____ AgUr ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ';
+   Rod(1).k17j5 = 'TiCu FeMn HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ ';
+   Rod(1).k25x1 = '____ ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ';
+   Rod(1).k26x5 = 'HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn ';
+   Rod(1).k2j56 = 'NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp ____ ____ AuPb ';
+   Rod(1).k34x2 = 'PbCu ____ ____ ____ AgHg TiFe FeTi HgAg ____ SnAu ____ CuPb ';
+   Rod(1).k56x4 = 'HgAu ____ SnPb ____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp ';
+   Rod(1).n25x6 = 'TiCu FeMn HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb ';
+   Rod(1).n26y5 = '____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn ';
+   Rod(1).n45y2 = 'HgTi ____ ____ UrAu ____ PbPb ____ AuUr NpSn ____ TiHg FeFe ';
+   Rod(1).n67x2 = '____ AuUr ____ ____ TiHg FeFe HgTi ____ SnNp UrAu ____ PbPb ';
+  Rod(1).j136y7 = '____ ____ SnPb UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ';
+  Rod(1).j167y2 = 'HgAu ____ ____ ____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp ';
+  Rod(1).j246y3 = 'HgHg PuFe SnTi ____ CuNp ____ ____ AuPb NpCu ____ ____ FePu ';
+  Rod(1).j26y34 = 'HgHg PuFe SnTi UrAg ____ ____ ____ AuPb NpCu ____ ____ FePu ';
+  Rod(1).j2k6x5 = 'HgHg PuFe ____ ____ CuNp PbAu ____ ____ ____ AgUr TiSn FePu ';
+  Rod(1).j2k6y3 = 'HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ ____ TiSn FePu ';
+  Rod(1).j346y5 = 'NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp ____ ____ AuPb ';
+  Rod(1).j3k5x4 = 'HgAu ____ SnPb UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp ';
+  Rod(1).k135x4 = '____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp HgAu ____ SnPb ';
+  Rod(1).k157x6 = 'HgHg PuFe SnTi ____ CuNp PbAu ____ ____ NpCu ____ ____ FePu ';
+  Rod(1).k1j6y7 = '____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp HgAu ____ ____ ';
+  Rod(1).k257x1 = 'NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ ';
+  Rod(1).k25x17 = '____ AgUr TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ ';
+  Rod(1).k2j5x1 = '____ ____ TiSn FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ';
+  Rod(1).k2j5y6 = 'NpCu ____ ____ FePu HgHg PuFe SnTi UrAg ____ ____ ____ AuPb ';
+  Rod(1).k345x2 = 'UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp HgAu ____ SnPb ';
+  Rod(1).n167x4 = 'HgAu ____ ____ UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ';
+  Rod(1).n345y7 = '____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp HgAu ____ SnPb ';
+ Rod(1).j2k56x4 = 'HgHg PuFe ____ ____ CuNp ____ ____ AuPb NpCu ____ TiSn FePu ';
+ Rod(1).j3k56x4 = 'HgTi ____ SnNp UrAu ____ ____ ____ AuUr NpSn ____ TiHg FeFe ';
+ Rod(1).k1j56y7 = '____ AuUr NpSn ____ TiHg FeFe HgTi ____ SnNp UrAu ____ ____ ';
+ Rod(1).k2j56y7 = 'NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp PbAu ____ ____ ';
+
+      Rod(2).j2 = '汞汞 钚铁 一一 一一 铜镎 铅金 一一 金铅 一一 银铀 一一 铁钚 ';
+      Rod(2).j3 = '汞锡 一一 锡汞 铀铁 一一 铅银 一一 金金 一一 银铅 一一 铁铀 ';
+      Rod(2).j5 = '铅铜 一一 金锡 一一 银汞 钒铁 铁钒 一一 一一 锡金 一一 铜铅 ';
+      Rod(2).j6 = '汞金 一一 锡铅 一一 铜铀 铅锡 一一 金汞 镎铁 一一 一一 铁镎 ';
+      Rod(2).k1 = '一一 铁铀 汞锡 一一 锡汞 铀铁 一一 铅银 一一 金金 一一 银铅 ';
+      Rod(2).k2 = '镎铜 一一 一一 铁钚 汞汞 钚铁 一一 铀银 一一 铅金 一一 金铅 ';
+      Rod(2).k5 = '铀铜 一一 铅锡 一一 金汞 镎铁 一一 一一 铁镎 汞金 一一 锡铅 ';
+      Rod(2).k6 = '汞银 一一 锡金 一一 铜铅 铅铜 一一 金锡 一一 一一 钒铁 铁钒 ';
+      Rod(2).n0 = '汞铜 一一 锡锡 一一 铜汞 铅铁 一一 金银 一一 银金 一一 铁铅 ';
+     Rod(2).j17 = '一一 一一 锡金 一一 铜铅 铅铜 一一 金锡 一一 银汞 钒铁 铁钒 ';
+     Rod(2).j23 = '汞汞 钚铁 一一 铀银 一一 铅金 一一 金铅 一一 银铀 一一 铁钚 ';
+     Rod(2).j25 = '铁铜 汞锰 一一 一一 锰汞 铜铁 铅钒 一一 一一 镎金 一一 钒铅 ';
+     Rod(2).j26 = '汞汞 钚铁 一一 一一 铜镎 铅金 一一 金铅 镎铜 一一 一一 铁钚 ';
+     Rod(2).j36 = '汞金 一一 锡铅 铀铜 一一 铅锡 一一 金汞 镎铁 一一 一一 铁镎 ';
+     Rod(2).j56 = '铀铜 一一 铅锡 一一 金汞 镎铁 银钒 一一 铁镎 一一 一一 锡铅 ';
+     Rod(2).k12 = '一一 银铀 一一 铁钚 汞汞 钚铁 一一 铀银 一一 铅金 一一 金铅 ';
+     Rod(2).k15 = '一一 铜铀 铅锡 一一 金汞 镎铁 一一 一一 铁镎 汞金 一一 锡铅 ';
+     Rod(2).k25 = '镎铜 一一 一一 铁钚 汞汞 钚铁 一一 一一 铜镎 铅金 一一 金铅 ';
+     Rod(2).k26 = '汞锰 一一 一一 锰汞 铜铁 铅钒 一一 金镎 一一 一一 钒铅 铁铜 ';
+     Rod(2).k34 = '铅铜 一一 金锡 一一 一一 钒铁 铁钒 汞银 一一 锡金 一一 铜铅 ';
+     Rod(2).k56 = '汞金 一一 锡铅 一一 铜铀 铅锡 一一 一一 镎铁 一一 钒银 铁镎 ';
+    Rod(2).j236 = '汞汞 钚铁 一一 铀银 一一 铅金 一一 金铅 镎铜 一一 一一 铁钚 ';
+    Rod(2).j256 = '铁铜 汞锰 一一 一一 锰汞 铜铁 铅钒 一一 金镎 一一 一一 钒铅 ';
+    Rod(2).j2k5 = '铁铜 汞锰 一一 一一 锰汞 铜铁 一一 一一 金镎 镎金 一一 钒铅 ';
+    Rod(2).j2k6 = '汞汞 钚铁 一一 一一 铜镎 铅金 一一 金铅 一一 一一 钒锡 铁钚 ';
+    Rod(2).j2y3 = '汞汞 钚铁 锡钒 一一 一一 铅金 一一 金铅 一一 银铀 一一 铁钚 ';
+    Rod(2).j3k5 = '镎铜 一一 钒锡 铁钚 一一 钚铁 一一 一一 铜镎 铅金 一一 金铅 ';
+    Rod(2).j3k6 = '汞钒 一一 锡镎 铀金 一一 铅铅 一一 金铀 一一 一一 钒汞 铁铁 ';
+    Rod(2).j5y6 = '铅铜 一一 金锡 一一 银汞 钒铁 铁钒 汞银 一一 一一 一一 铜铅 ';
+    Rod(2).k125 = '一一 银铀 一一 铁钚 汞汞 钚铁 一一 一一 铜镎 铅金 一一 金铅 ';
+    Rod(2).k1j5 = '一一 金铀 镎锡 一一 钒汞 铁铁 汞钒 一一 一一 铀金 一一 铅铅 ';
+    Rod(2).k1j6 = '一一 钚铁 锡钒 一一 铜镎 铅金 一一 金铅 镎铜 一一 一一 铁钚 ';
+    Rod(2).k256 = '汞锰 一一 一一 锰汞 铜铁 铅钒 一一 一一 镎金 一一 钒铅 铁铜 ';
+    Rod(2).k2j5 = '镎铜 一一 一一 铁钚 汞汞 钚铁 锡钒 一一 一一 铅金 一一 金铅 ';
+    Rod(2).k2j6 = '汞锰 一一 一一 锰汞 铜铁 铅钒 一一 金镎 镎金 一一 一一 铁铜 ';
+    Rod(2).k2x1 = '一一 一一 钒锡 铁钚 汞汞 钚铁 一一 铀银 一一 铅金 一一 金铅 ';
+    Rod(2).k6x5 = '汞银 一一 锡金 一一 铜铅 铅铜 一一 一一 一一 银汞 钒铁 铁钒 ';
+    Rod(2).n167 = '镎铜 一一 一一 铁钚 一一 钚铁 锡钒 一一 铜镎 铅金 一一 金铅 ';
+    Rod(2).n345 = '一一 钚铁 一一 一一 铜镎 铅金 一一 金铅 镎铜 一一 钒锡 铁钚 ';
+    Rod(2).n5y2 = '汞锰 一一 一一 锰汞 铜铁 一一 一一 金镎 镎金 一一 钒铅 铁铜 ';
+    Rod(2).n6x2 = '铁铜 汞锰 一一 一一 锰汞 铜铁 铅钒 一一 金镎 镎金 一一 一一 ';
+   Rod(2).j17k2 = '一一 一一 一一 锰铁 铜钒 铅银 一一 金金 一一 银铅 钒铜 铁锰 ';
+   Rod(2).j17y2 = '汞银 一一 一一 一一 铜铅 铅铜 一一 金锡 一一 银汞 钒铁 铁钒 ';
+   Rod(2).j23k6 = '汞汞 钚铁 一一 铀银 一一 铅金 一一 金铅 一一 一一 钒锡 铁钚 ';
+   Rod(2).j25y6 = '钒铜 铁锰 一一 一一 锡汞 锰铁 铜钒 铅银 一一 一一 一一 银铅 ';
+   Rod(2).j26y3 = '汞汞 钚铁 锡钒 一一 一一 铅金 一一 金铅 镎铜 一一 一一 铁钚 ';
+   Rod(2).j2k34 = '钒铜 铁锰 一一 一一 一一 锰铁 铜钒 铅银 一一 金金 一一 银铅 ';
+   Rod(2).j2k56 = '汞汞 钚铁 一一 一一 铜镎 铅金 一一 一一 镎铜 一一 钒锡 铁钚 ';
+   Rod(2).j34k6 = '汞锡 一一 锡汞 锰铁 铜钒 一一 一一 金金 一一 一一 钒铜 铁锰 ';
+   Rod(2).j56y7 = '铀铜 一一 铅锡 一一 金汞 镎铁 银钒 一一 铁镎 汞金 一一 一一 ';
+   Rod(2).k12j5 = '一一 银铀 一一 铁钚 汞汞 钚铁 锡钒 一一 一一 铅金 一一 金铅 ';
+   Rod(2).k17j5 = '钒铜 铁锰 汞锡 一一 锡汞 锰铁 铜钒 一一 一一 金金 一一 一一 ';
+   Rod(2).k25x1 = '一一 一一 钒锡 铁钚 汞汞 钚铁 一一 一一 铜镎 铅金 一一 金铅 ';
+   Rod(2).k26x5 = '汞锡 一一 一一 锰铁 铜钒 铅银 一一 一一 一一 银铅 钒铜 铁锰 ';
+   Rod(2).k2j56 = '镎铜 一一 一一 铁钚 汞汞 钚铁 锡钒 一一 铜镎 一一 一一 金铅 ';
+   Rod(2).k34x2 = '铅铜 一一 一一 一一 银汞 钒铁 铁钒 汞银 一一 锡金 一一 铜铅 ';
+   Rod(2).k56x4 = '汞金 一一 锡铅 一一 铜铀 一一 一一 金汞 镎铁 一一 钒银 铁镎 ';
+   Rod(2).n25x6 = '钒铜 铁锰 汞锡 一一 一一 锰铁 铜钒 铅银 一一 一一 一一 银铅 ';
+   Rod(2).n26y5 = '一一 一一 锡汞 锰铁 铜钒 铅银 一一 一一 一一 银铅 钒铜 铁锰 ';
+   Rod(2).n45y2 = '汞钒 一一 一一 铀金 一一 铅铅 一一 金铀 镎锡 一一 钒汞 铁铁 ';
+   Rod(2).n67x2 = '一一 金铀 一一 一一 钒汞 铁铁 汞钒 一一 锡镎 铀金 一一 铅铅 ';
+  Rod(2).j136y7 = '一一 一一 锡铅 铀铜 一一 铅锡 一一 金汞 镎铁 银钒 一一 铁镎 ';
+  Rod(2).j167y2 = '汞金 一一 一一 一一 铜铀 铅锡 一一 金汞 镎铁 一一 钒银 铁镎 ';
+  Rod(2).j246y3 = '汞汞 钚铁 锡钒 一一 铜镎 一一 一一 金铅 镎铜 一一 一一 铁钚 ';
+  Rod(2).j26y34 = '汞汞 钚铁 锡钒 铀银 一一 一一 一一 金铅 镎铜 一一 一一 铁钚 ';
+  Rod(2).j2k6x5 = '汞汞 钚铁 一一 一一 铜镎 铅金 一一 一一 一一 银铀 钒锡 铁钚 ';
+  Rod(2).j2k6y3 = '汞汞 钚铁 锡钒 一一 一一 铅金 一一 金铅 一一 一一 钒锡 铁钚 ';
+  Rod(2).j346y5 = '镎铜 一一 钒锡 铁钚 汞汞 钚铁 一一 一一 铜镎 一一 一一 金铅 ';
+  Rod(2).j3k5x4 = '汞金 一一 锡铅 铀铜 一一 一一 一一 金汞 镎铁 银钒 一一 铁镎 ';
+  Rod(2).k135x4 = '一一 铜铀 铅锡 一一 一一 镎铁 一一 钒银 铁镎 汞金 一一 锡铅 ';
+  Rod(2).k157x6 = '汞汞 钚铁 锡钒 一一 铜镎 铅金 一一 一一 镎铜 一一 一一 铁钚 ';
+  Rod(2).k1j6y7 = '一一 铜铀 铅锡 一一 金汞 镎铁 一一 钒银 铁镎 汞金 一一 一一 ';
+  Rod(2).k257x1 = '镎铜 一一 钒锡 铁钚 汞汞 钚铁 一一 一一 铜镎 铅金 一一 一一 ';
+  Rod(2).k25x17 = '一一 银铀 钒锡 铁钚 汞汞 钚铁 一一 一一 铜镎 铅金 一一 一一 ';
+  Rod(2).k2j5x1 = '一一 一一 钒锡 铁钚 汞汞 钚铁 锡钒 一一 一一 铅金 一一 金铅 ';
+  Rod(2).k2j5y6 = '镎铜 一一 一一 铁钚 汞汞 钚铁 锡钒 铀银 一一 一一 一一 金铅 ';
+  Rod(2).k345x2 = '铀铜 一一 一一 一一 金汞 镎铁 银钒 一一 铁镎 汞金 一一 锡铅 ';
+  Rod(2).n167x4 = '汞金 一一 一一 铀铜 一一 铅锡 一一 金汞 镎铁 银钒 一一 铁镎 ';
+  Rod(2).n345y7 = '一一 铜铀 一一 一一 金汞 镎铁 一一 钒银 铁镎 汞金 一一 锡铅 ';
+ Rod(2).j2k56x4 = '汞汞 钚铁 一一 一一 铜镎 一一 一一 金铅 镎铜 一一 钒锡 铁钚 ';
+ Rod(2).j3k56x4 = '汞钒 一一 锡镎 铀金 一一 一一 一一 金铀 镎锡 一一 钒汞 铁铁 ';
+ Rod(2).k1j56y7 = '一一 金铀 镎锡 一一 钒汞 铁铁 汞钒 一一 锡镎 铀金 一一 一一 ';
+ Rod(2).k2j56y7 = '镎铜 一一 一一 铁钚 汞汞 钚铁 锡钒 一一 铜镎 铅金 一一 一一 ';
+
+% latin methods
+
+  function rtv = svBj (qp)
+    rtv = [qp(51:end), qp(1:50)];
+  endfunction
+
+  function rtv = svFn (qp)
+    rtv = [qp(26:end), qp(1:25)];
+  endfunction
+
+  function rtv = svCn (qp)
+    rtv = qp;
+  endfunction
+
+  function rtv = svGn (qp)
+    rtv = [qp(36:end), qp(1:35)];
+  endfunction
+
+  function rtv = svDn (qp)
+    rtv = [qp(11:end), qp(1:10)];
+  endfunction
+
+  function rtv = svAn (qp)
+    rtv = [qp(46:end), qp(1:45)];
+  endfunction
+
+  function rtv = svEn (qp)
+    rtv = [qp(21:end), qp(1:20)];
+  endfunction
+
+  function rtv = svBn (qp)
+    rtv = [qp(56:end), qp(1:55)];
+  endfunction
+
+  function rtv = svFk (qp)
+    rtv = [qp(31:end), qp(1:30)];
+  endfunction
+
+% hanzi methods
+
+  function rtv = zhBj (qp)
+    rtv = [qp(71:end), qp(1:70)];
+  endfunction
+
+  function rtv = zhFn (qp)
+    rtv = [qp(36:end), qp(1:35)];
+  endfunction
+
+  function rtv = zhCn (qp)
+    rtv = qp;
+  endfunction
+
+  function rtv = zhGn (qp)
+    rtv = [qp(50:end), qp(1:49)];
+  endfunction
+
+  function rtv = zhDn (qp)
+    rtv = [qp(15:end), qp(1:14)];
+  endfunction
+
+  function rtv = zhAn (qp)
+    rtv = [qp(64:end), qp(1:63)];
+  endfunction
+
+  function rtv = zhEn (qp)
+    rtv = [qp(29:end), qp(1:28)];
+  endfunction
+
+  function rtv = zhBn (qp)
+    rtv = [qp(78:end), qp(1:77)];
+  endfunction
+
+  function rtv = zhFk (qp)
+    rtv = [qp(43:end), qp(1:42)];
+  endfunction
+
+% format methods
+
+  function rtv = Chronotis
+    rtv = [(datestr (time, 30)), ' '];
+  endfunction
+
+  function rtv = frmt (qp)
+    rtv = printf ('\t%s\n', qp);
+  endfunction
+
+% latin tunings
+
+  function svBEADGCF (ks, qp)
+    frmt ([ks, '-beadgcf-sv', Chronotis]);
+    frmt (svFn (qp));
+    frmt (svCn (qp));
+    frmt (svGn (qp));
+    frmt (svDn (qp));
+    frmt (svAn (qp));
+    frmt (svEn (qp));
+    frmt (svBn (qp));
+  endfunction
+
+  function svCGDAE (ks, qp)
+    frmt ([ks, '-cgdae-sv', Chronotis]);
+    frmt (svEn (qp));
+    frmt (svAn (qp));
+    frmt (svDn (qp));
+    frmt (svGn (qp));
+    frmt (svCn (qp));
+  endfunction
+
+  function svEADGBE (ks, qp)
+    frmt ([ks, '-eadgbe-sv', Chronotis]);
+    frmt (svEn (qp));
+    frmt (svBn (qp));
+    frmt (svGn (qp));
+    frmt (svDn (qp));
+    frmt (svAn (qp));
+    frmt (svEn (qp));
+  endfunction
+
+  function svFkBjDn (ks, qp)
+    frmt ([ks, '-fkbjdn-sv', Chronotis]);
+    frmt (svDn (qp));
+    frmt (svBj (qp));
+    frmt (svFk (qp));
+    frmt (svDn (qp));
+    frmt (svBj (qp));
+    frmt (svFk (qp));
+  endfunction
+
+% hanzi tunings
+
+  function zhBEADGCF (ks, qp)
+    frmt ([ks, '-beadgcf-zh', Chronotis]);
+    frmt (zhFn (qp));
+    frmt (zhCn (qp));
+    frmt (zhGn (qp));
+    frmt (zhDn (qp));
+    frmt (zhAn (qp));
+    frmt (zhEn (qp));
+    frmt (zhBn (qp));
+  endfunction
+
+  function zhCGDAE (ks, qp)
+    frmt ([ks, '-cgdae-zh', Chronotis]);
+    frmt (zhEn (qp));
+    frmt (zhAn (qp));
+    frmt (zhDn (qp));
+    frmt (zhGn (qp));
+    frmt (zhCn (qp));
+  endfunction
+
+  function zhEADGBE (ks, qp)
+    frmt ([ks, '-eadgbe-zh', Chronotis]);
+    frmt (zhEn (qp));
+    frmt (zhBn (qp));
+    frmt (zhGn (qp));
+    frmt (zhDn (qp));
+    frmt (zhAn (qp));
+    frmt (zhEn (qp));
+  endfunction
+
+  function zhFkBjDn (ks, qp)
+    frmt ([ks, '-fkbjdn-zh', Chronotis]);
+    frmt (zhDn (qp));
+    frmt (zhBj (qp));
+    frmt (zhFk (qp));
+    frmt (zhDn (qp));
+    frmt (zhBj (qp));
+    frmt (zhFk (qp));
+  endfunction
+
+% latin loops
+
+  function latinBass (qp)
+    for [v, k] = qp
+      svBEADGCF (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+  function latinCello (qp)
+    for [v, k] = qp
+      svCGDAE (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+  function latinGuitar (qp)
+    for [v, k] = qp
+      svEADGBE (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+  function latinMaj3rds (qp)
+    for [v, k] = qp
+      svFkBjDn (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+% hanzi loops
+
+  function hanziBass (qp)
+    for [v, k] = qp
+      zhBEADGCF (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+  function hanziCello (qp)
+    for [v, k] = qp
+      zhCGDAE (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+  function hanziGuitar (qp)
+    for [v, k] = qp
+      zhEADGBE (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+  function hanziMaj3rds (qp)
+    for [v, k] = qp
+      zhFkBjDn (k, v);
+      disp ('');
+    endfor
+  endfunction
+
+% screen output
+
+disp ('');
+
+hanziBass (Rod(2));
+
+
